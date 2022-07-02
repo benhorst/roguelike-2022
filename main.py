0f9f99611f4387f0b85c3bf18bf3609283672a99
benhorst <benpub@benhorst.com>
2022-07-02 09:20:00 -0700

    paint the screen once

--- main.py
+++ main.py
@@ -2,8 +2,28 @@
 import tcod
 
 
-def main():
-    print("Hello World!")
+def main() -> None:
+    """Entry point for the program."""
+    screen_width = 80
+    screen_height = 50
+
+    tileset = tcod.tileset.load_tilesheet("tilesheet.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Roguelike",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=1, y=1, string="@");
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
 
 
 if __name__ == "__main__":
